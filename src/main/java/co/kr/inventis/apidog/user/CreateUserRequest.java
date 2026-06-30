e3d0b23dc414b70c53c4e2feb15b03b26538ec93
hsk741 <hsk741@gmail.com>
2026-06-30 13:45:57 +0900

    1. gradle version 2. validation

--- src/main/java/co/kr/inventis/apidog/user/CreateUserRequest.java
+++ src/main/java/co/kr/inventis/apidog/user/CreateUserRequest.java
@@ -1,7 +1,15 @@
 package co.kr.inventis.apidog.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CreateUserRequest(
+
+    @NotBlank
+    @Size(max = 100)
     String name,
 
+    @Email
     String email
 ) {}
--- src/main/java/co/kr/inventis/apidog/user/CreateUserResponse.java
+++ src/main/java/co/kr/inventis/apidog/user/CreateUserResponse.java
@@ -1,15 +1,8 @@
 package co.kr.inventis.apidog.user;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
 public record CreateUserResponse(
 
-    @NotBlank
-    @Size(max = 100)
     String name,
 
-    @Email
     String email
 ) {}
